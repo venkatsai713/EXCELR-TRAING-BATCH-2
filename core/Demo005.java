b26a2155691acae71744a103424dd0e3ef8b369b
VenkatSai713 <venkatsaiyerragunta456@gmail.com>
2025-01-26 23:06:46 +0530

    Matching username and password

---
+++ core/Demo005.java
@@ -0,0 +1,12 @@
+public class Demo005 {
+    public static void main(String[]args){
+        String userid = "user001";
+        String password = "user001";
+
+        while((userid == "user001") && (password == "user001")){
+                System.out.println(" Hello User,Happy New year");
+                break;
+        }
+    }
+    
+}
