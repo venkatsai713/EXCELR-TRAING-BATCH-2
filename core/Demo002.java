2ddb02b6b2c1ff474dc039a142458a1014633c45
VenkatSai713 <venkatsaiyerragunta456@gmail.com>
2025-01-26 23:05:56 +0530

    Greatest of 2 Numbers

---
+++ core/Demo002.java
@@ -0,0 +1,12 @@
+public class Demo002 {
+    public static void main(String[]main){
+        int a = 20;
+        int b = 10;
+        if(a>b){
+            System.out.println("A is greater then B ");
+        }else{
+                System.out.println("A is less than B ");
+            }
+        }
+    }
+
