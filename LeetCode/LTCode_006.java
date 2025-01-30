2958600b69f261947fd52e513e179cb1070c313f
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-01-30 10:26:16 +0530

    LTCode_006

---
+++ LeetCode/LTCode_006.java
@@ -0,0 +1,28 @@
+class LTCode_006 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        LTCode_006 solution = new LTCode_006();
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int k = solution.removeDuplicates(nums);
+        System.out.println("Number of unique elements: " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
