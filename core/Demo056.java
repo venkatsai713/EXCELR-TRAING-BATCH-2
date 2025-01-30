4302bb6564b6019c04b96550ac5408f17e2c8b56
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-01-30 09:38:16 +0530

    Program to print sum of cubes of a number

---
+++ core/Demo056.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(); 
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10; 
+            sum += digit * digit * digit; 
+            num /= 10; 
+        }
+
+        System.out.println("Sum of cubes of digits: " + sum);
+        sc.close(); 
+    }
+}
