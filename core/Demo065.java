69aeed8792a56c404a438eab79718ab66579b06b
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-02-05 10:06:11 +0530

    Java program to print array elements

---
+++ core/Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50}; 
+
+        System.out.print("Array elements: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
