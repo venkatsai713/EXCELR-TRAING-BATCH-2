26a68f93280f6ec416e90815d381c3de31949a1e
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-02-12 09:37:17 +0530

    Program to print the 5 times table

---
+++ core/Demo050.java
@@ -0,0 +1,9 @@
+//Write a Java program to print the 5 times table
+
+public class Demo050 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(5 + " x " + i + " = " + 5 * i);
+        }
+    }
+}
